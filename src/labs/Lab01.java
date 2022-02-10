9b92553c2b5df72fa85a309359415cb6a6c2ea4f
1Door <89485167+1Door@users.noreply.github.com>
2022-02-10 15:41:19 -0600

    h

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -16,6 +16,13 @@ public class Lab01 {
 
 	
 	public static void problem01() {
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose 2 positive integers");
+		int input1 =  inKey.nextInt();
+		int input2 =  inKey.nextInt();
+		double total = Math.pow(input1, input2);
+		System.out.println(input1+"^"+ input2 + " = " + total + "\n");
+		
 		
 		
 		
@@ -25,8 +32,11 @@ public class Lab01 {
 	
 	
 	public static void problem02() {
-		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose a positive integer");
+		int input =  inKey.nextInt();
+		double num1 = Math.sqrt(input);
+		System.out.println(num1 + "\n");
 		
 	}
 
@@ -35,8 +45,20 @@ public class Lab01 {
 
 	public static void problem03() {
 		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose 2 positive integers");
+		double input1 =  inKey.nextDouble();
+		double input2 =  inKey.nextDouble();
+		double input1bac =  input1;
+		double input2bac =  input2;
+		input1 = input1 * input1;
+		input2 = input2 * input2;
+		double total = input1 + input2;
 		
-		
+		total =	Math.sqrt(total);
+		System.out.println("Length of side A: " + input1bac);
+		System.out.println("Length of side B: " + input2bac);
+		System.out.println("Hypotenuse: " + total);
 
 	}
 
@@ -45,7 +67,27 @@ public class Lab01 {
 	
 	public static void problem04() {
 		
-		
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Enter a Integer (Enter 0 to End.)");
+		int loop = 1; 
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		while (loop > 0) { // Makes sure that  the loop is still running
+			
+			int input = inKey.nextInt();
+			System.out.println("You entered: " + input);
+			
+			max = Math.max(max, input);
+			min = Math.min(min, input);
+				
+			if (input == 0) {
+				loop = 0;
+			}
+
+		}
+	
+		 System.out.println("Max = " + max);
+		 System.out.println("Min = " + min);
 		
 	}
 	
