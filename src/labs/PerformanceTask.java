9ea82f7e8be94594f0278f1dbed47a601ba000bf
1Door <89485167+1Door@users.noreply.github.com>
2022-03-21 17:07:51 -0500

    Update PerformanceTask.java

--- src/labs/PerformanceTask.java
+++ src/labs/PerformanceTask.java
@@ -10,36 +10,36 @@ public class PerformanceTask {
 		double x = 0;
 		double y =  0;
 	
-		System.out.println("Enter Launch Velocity (m/s)");
+		System.out.println("Enter Launch Velocity (m/s)"); //gets input
 		double launchVelocity = inKey.nextDouble();
-		System.out.println("Enter Launch Angle (degrees)");
+		System.out.println("Enter Launch Angle (degrees)"); //gets input
 		double launchAngle = inKey.nextDouble();
 		
 		int time = 0;
-		int killSwitch = 0;
+		int killSwitch = 0; //killswitch
 		
 		
-		double lAcos = Math.cos(Math.toRadians(launchAngle));
+		double lAcos = Math.cos(Math.toRadians(launchAngle)); //gets cos
 		
 		
-		double lVsin = Math.sin(Math.toRadians(launchAngle));
-		
+		double lVsin = Math.sin(Math.toRadians(launchAngle)); //gets sin
+	
 	
 		System.out.println("\n\nProjectile's Path\n\n");
 		while (killSwitch != 2)
 		
 		{
 		
-			x = lAcos * time * launchVelocity;
-			y = lVsin * time * launchVelocity -0.5 * 9.8 * (time * time);
+			x = lAcos * time * launchVelocity; //does the x value
+			y = lVsin * time * launchVelocity -0.5 * 9.8 * (time * time); //does the y value math
 			
 			if (y <= 0) {
 				y = 0;
 				killSwitch++;
 			}
-			System.out.println("Time: " + time + "\nx-pos: "+ x + "\ny-pos: " + y + "\n");
+			System.out.println("Time: " + time + "\nx-pos: "+ x + "\ny-pos: " + y + "\n"); //prints
 			
-			time++;
+			time++; //adds to time
 			
 			
 			
